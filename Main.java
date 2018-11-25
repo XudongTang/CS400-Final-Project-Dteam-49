c5a2a9afd6fe63f3ce76968204b1cb6a46f635dd
Xudong Tang <44549914+XudongTang@users.noreply.github.com>
2018-11-24 19:10:49 -0600

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,429 @@
+package application;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.event.Event;
+import javafx.event.EventHandler;
+import javafx.geometry.Pos;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.SelectionMode;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
+import javafx.stage.FileChooser;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import javafx.geometry.Insets;
+
+public class Main extends Application implements EventHandler<ActionEvent>{
+	
+	private FoodData foods = new FoodData();
+	private ObservableList<String> items1 = FXCollections.observableArrayList();
+	private ObservableList<String> items2 = FXCollections.observableArrayList();
+	private ListView<String> list1 = new ListView<String>();
+	private ListView<String> list2 = new ListView<String>();
+	private Label label1 = new Label("Current Food List");
+	private Label label2 = new Label("Current Meal List");
+	private Label label3 = new Label("Number of Food: (Replace with actual list)");
+	private VBox vbox = new VBox();
+	private Button btn1 = new Button("Load Additional Food");
+	private Button btn2 = new Button("Add Food");
+	private Button btn3 = new Button("Name Filter");
+	private Button btn4 = new Button("Nutrient Filter");
+	private Button btn5 = new Button("Save");
+	private Button btn6 = new Button("Anzlyze Meal");
+	private Button btn7 = new Button("Add to Meal List");
+	private ArrayList <String> uploadList = new ArrayList<String> ();
+	private List <FoodItem> mealList = new ArrayList<FoodItem> ();
+
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			Group root = new Group();
+			Scene scene = new Scene (root, 800, 600);
+			primaryStage.setScene(scene);
+			primaryStage.setTitle("DEMO");
+			scene.setFill(Color.WHITE);
+			
+			
+			
+			//replace with actual food list
+//			items1 = FXCollections.observableArrayList (
+//			    "Apple", "Beans", "Chicken", "Danelion Green", "Eggs", "Flax Seed"
+//			    , "Garlic", "Hominy", "Ice Cream", "Jicama", "Kale", "Lemon Juice", 
+//			    "Millet", "Nuts", "Olive Oil", "Pineapple", "Quinoa", "Raspberries", 
+//			    "Salmon", "Tomatoes", "Umami", "Vinegar", "Wild Rice", "Xigua", 
+////			    "Yogurt", "Zucchini");
+//			ObservableList<String> items2 =FXCollections.observableArrayList (
+//				"Apple", "Beans", "Chicken", "Danelion Green", "Eggs", "Flax Seed"
+//				, "Garlic", "Hominy");
+			
+			
+			 btn1.setOnAction(new EventHandler<ActionEvent>() {
+				 @Override
+				 public void handle(ActionEvent e) {
+					 final Stage dialog = new Stage();
+					 dialog.initModality(Modality.APPLICATION_MODAL);
+					 dialog.initOwner(primaryStage);
+					 GridPane load = new GridPane();
+					 load.setAlignment(Pos.CENTER);
+					 load.setHgap(10);
+					 load.setVgap(10);
+					 load.setPadding(new Insets(0, 0, 0, 0));
+					 Text scenetitle = new Text("Load Additional Food");
+					 scenetitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 15));
+					 load.add(scenetitle, 0, 0, 2, 1);
+					 Label userName = new Label("File:");
+		       		 load.add(userName, 0, 1);
+					 TextField userTextField = new TextField();
+					 load.add(userTextField, 1, 1);
+					 Button file = new Button ("Choose File");
+					 Button upload = new Button ("UPLOAD");
+					 load.add(file, 1, 2);
+					 load.add(upload, 1, 3);
+					 file.setOnAction(new EventHandler<ActionEvent>() {
+						 public void handle(ActionEvent x) {
+							 FileChooser fileChooser = new FileChooser();
+							 fileChooser.setTitle("Open Resource File");
+							 File file = fileChooser.showOpenDialog(dialog);
+							 if (file != null) {
+								 userTextField.setText(file.toString());
+							 }
+						 }
+					 });
+					 upload.setOnAction(new EventHandler<ActionEvent>() {
+						public void handle(ActionEvent x) {
+							if (!userTextField.getText().isEmpty()) {
+								foods.loadFoodItems(userTextField.getText());
+								uploadList = convert(foods.getAllFoodItems());
+								items1 = FXCollections.observableArrayList(uploadList);
+								list1.setItems(items1);
+							}
+
+							dialog.close();
+							// TODO
+						}
+					 });
+					 Scene dialogScene = new Scene(load, 300, 200);
+		             dialog.setScene(dialogScene);
+		             dialog.show();
+				 }
+			 });
+			try {
+			 btn2.setOnAction(new EventHandler<ActionEvent>() {
+				 @Override
+				 public void handle(ActionEvent e) {
+					 final Stage dialog = new Stage();
+					 dialog.initModality(Modality.APPLICATION_MODAL);
+					 dialog.initOwner(primaryStage);
+					 GridPane load = new GridPane();
+					 load.setAlignment(Pos.CENTER);
+					 load.setHgap(10);
+					 load.setVgap(10);
+					 load.setPadding(new Insets(0, 0, 0, 0));
+					 
+					 Label warn = new Label("Invalid input");
+					 Text scenetitle = new Text("ADD FOOD");
+					 scenetitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 15));
+					 load.add(scenetitle, 0, 0, 2, 1);
+					 Label id = new Label("ID (24 digits):");
+		       		 load.add(id, 0, 1);
+					 TextField userTextField1 = new TextField();
+					 load.add(userTextField1, 1, 1);
+					 Label name = new Label("Name:");
+		       		 load.add(name, 0, 2);
+					 TextField userTextField2 = new TextField();
+					 load.add(userTextField2, 1, 2);
+					 Label calorie = new Label("Calories:");
+		       		 load.add(calorie, 0, 3);
+					 TextField userTextField3 = new TextField();
+					 load.add(userTextField3, 1, 3);
+					 Label fat = new Label("Fat:");
+		       		 load.add(fat, 0, 4);
+					 TextField userTextField4 = new TextField();
+					 load.add(userTextField4, 1, 4);
+					 Label carb = new Label("Carbohydrate:");
+		       		 load.add(carb, 0, 5);
+					 TextField userTextField5 = new TextField();
+					 load.add(userTextField5, 1, 5);
+					 Label fiber = new Label("Fiber:");
+		       		 load.add(fiber, 0, 6);
+					 TextField userTextField6 = new TextField();
+					 load.add(userTextField6, 1, 6);
+					 Label protein = new Label("Protein:");
+		       		 load.add(protein, 0, 7);
+					 TextField userTextField7 = new TextField();
+					 load.add(userTextField7, 1, 7);
+					 Button upload = new Button ("UPLOAD");
+					 load.add(upload, 0, 8);
+					 upload.setOnAction(new EventHandler<ActionEvent>() {
+						 public void handle(ActionEvent x) {
+							 String id1 = userTextField1.getText();
+							 String name1 = userTextField2.getText();
+							 FoodItem newFood = null;
+							 if (name1 != null && id1 != null && id1.length() == 24) {
+								 newFood = new FoodItem(id1, name1);
+								 newFood.addNutrient("calories", Double.parseDouble(userTextField3.getText()));
+								 newFood.addNutrient("fat", Double.parseDouble(userTextField4.getText()));
+								 newFood.addNutrient("carbohydrate", Double.parseDouble(userTextField5.getText()));
+								 newFood.addNutrient("fiber", Double.parseDouble(userTextField6.getText()));
+								 newFood.addNutrient("protein", Double.parseDouble(userTextField7.getText()));
+								 foods.addFoodItem(newFood);
+								 dialog.close();
+							 } else {
+								 load.add(warn, 1, 8);
+							 }
+							 update (foods.getAllFoodItems());
+						 }
+					 });
+					 Scene dialogScene = new Scene(load, 400, 400);
+		             dialog.setScene(dialogScene);
+		             dialog.show();
+				 }
+			 });
+			} catch (NullPointerException e) {
+				System.out.println("Error");
+			}
+			 btn3.setOnAction(new EventHandler<ActionEvent>() {
+				 @Override
+				 public void handle(ActionEvent e) {
+					 final Stage dialog = new Stage();
+					 dialog.initModality(Modality.APPLICATION_MODAL);
+					 dialog.initOwner(primaryStage);
+					 GridPane load = new GridPane();
+					 load.setAlignment(Pos.CENTER);
+					 load.setHgap(10);
+					 load.setVgap(10);
+					 load.setPadding(new Insets(0, 0, 0, 0));
+					 Text scenetitle = new Text("Name Filter");
+					 scenetitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 15));
+					 load.add(scenetitle, 0, 0, 2, 1);
+					 TextField userTextField = new TextField();
+					 load.add(userTextField, 0, 1);
+					 Button upload = new Button ("SEARCH");
+					 load.add(upload, 0, 2);
+					 upload.setOnAction(new EventHandler<ActionEvent>() {
+						 public void handle(ActionEvent x) {
+							 //TODO
+							 dialog.close();
+						 }
+					 });
+					 Scene dialogScene = new Scene(load, 300, 200);
+		             dialog.setScene(dialogScene);
+		             dialog.show();
+				 }
+			 });
+			 btn4.setOnAction(new EventHandler<ActionEvent>() {
+				 @Override
+				 public void handle(ActionEvent e) {
+					 final Stage dialog = new Stage();
+					 dialog.initModality(Modality.APPLICATION_MODAL);
+					 dialog.initOwner(primaryStage);
+					 GridPane load = new GridPane();
+					 load.setAlignment(Pos.CENTER);
+					 load.setHgap(10);
+					 load.setVgap(10);
+					 load.setPadding(new Insets(0, 0, 0, 0));
+					 Text scenetitle = new Text("Nutrient Filter");
+					 scenetitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 15));
+					 load.add(scenetitle, 0, 0, 2, 1);
+					 TextField userTextField = new TextField();
+					 load.add(userTextField, 0, 1);
+					 Button upload = new Button ("SEARCH");
+					 load.add(upload, 0, 2);
+					 upload.setOnAction(new EventHandler<ActionEvent>() {
+						 public void handle(ActionEvent x) {
+							 //TODO
+							 dialog.close();
+						 }
+					 });
+					 Scene dialogScene = new Scene(load, 300, 200);
+		             dialog.setScene(dialogScene);
+		             dialog.show();
+				 }
+			 });
+			 btn5.setOnAction(new EventHandler<ActionEvent>() {
+				 @Override
+				 public void handle(ActionEvent e) {
+					 //TODO
+				 }
+			 });
+			 btn6.setOnAction(new EventHandler<ActionEvent>() {
+				 @Override
+				 public void handle(ActionEvent e) {
+					 final Stage dialog = new Stage();
+					 dialog.initModality(Modality.APPLICATION_MODAL);
+					 dialog.initOwner(primaryStage);
+					 GridPane load = new GridPane();
+					 load.setAlignment(Pos.CENTER);
+					 load.setHgap(10);
+					 load.setVgap(10);
+					 load.setPadding(new Insets(0, 0, 0, 0));
+					 Text scenetitle = new Text("NUTRIENT ANAYLZE: UNAVAILABLE IN DEMO");
+					 scenetitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 15));
+					 load.add(scenetitle, 0, 0, 2, 1);
+					 Scene dialogScene = new Scene(load, 300, 200);
+		             dialog.setScene(dialogScene);
+		             dialog.show();
+				 }
+			 });
+			 btn7.setOnAction(new EventHandler<ActionEvent>() {
+				 @Override
+				 public void handle(ActionEvent e) {
+					 ObservableList<Integer> index = list1.getSelectionModel().getSelectedIndices();
+					 for (int i = 0; i < index.size(); ++i) {
+						 mealList.add(foods.getAllFoodItems().get(index.get(i)));
+					 }
+					 mealList = sort(mealList);
+					 items2 = FXCollections.observableArrayList(convert(mealList));
+					 list2.setItems(items2);
+				 }
+				
+			 });
+//			 list1.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
+//				list1.setOnMouseClicked(new EventHandler<Event>() {
+//					@Override
+//	                public void handle(Event event) {
+//						ObservableList<String> selectedItems =  list1.getSelectionModel()
+//								.getSelectedItems();
+//						
+//					}
+//			});
+			
+			list1.setItems(items1);
+			list1.setPrefWidth(200);
+			list1.setPrefHeight(400);
+			list1.setTranslateX(50);
+			list1.setTranslateY(50);
+			list1.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
+//			list1.setOnMouseClicked(new EventHandler<Event> () {
+//				@Override
+//				public void handle (Event e) {
+//					
+//				}
+//			});
+			
+			list2.setItems(items2);
+			list2.setPrefWidth(200);
+			list2.setPrefHeight(400);
+			list2.setTranslateX(550);
+			list2.setTranslateY(50);
+			
+			label1.setTranslateX(50);
+			label1.setTranslateY(30);
+			
+			label2.setTranslateX(550);
+			label2.setTranslateY(30);
+			
+			label3.setTranslateX(50);
+			label3.setTranslateY(510);
+			
+			vbox.setPrefSize(200, 50);
+			btn1.setMinHeight(vbox.getPrefHeight());
+			btn1.setMinWidth(vbox.getPrefWidth());
+			btn1.setTranslateX(300);
+			btn1.setTranslateY(50);
+			
+			btn2.setMinHeight(vbox.getPrefHeight());
+			btn2.setMinWidth(vbox.getPrefWidth());
+			btn2.setTranslateX(300);
+			btn2.setTranslateY(150);
+			
+			btn3.setMinHeight(vbox.getPrefHeight());
+			btn3.setMinWidth(vbox.getPrefWidth());
+			btn3.setTranslateX(300);
+			btn3.setTranslateY(250);
+			
+			btn4.setMinHeight(vbox.getPrefHeight());
+			btn4.setMinWidth(vbox.getPrefWidth());
+			btn4.setTranslateX(300);
+			btn4.setTranslateY(350);
+			
+			btn5.setMinHeight(vbox.getPrefHeight());
+			btn5.setMinWidth(vbox.getPrefWidth());
+			btn5.setTranslateX(300);
+			btn5.setTranslateY(450);
+			
+			btn6.setMinHeight(vbox.getPrefHeight());
+			btn6.setMinWidth(vbox.getPrefWidth());
+			btn6.setTranslateX(550);
+			btn6.setTranslateY(450);
+			
+			btn7.setMinHeight(vbox.getPrefHeight());
+			btn7.setMinWidth(vbox.getPrefWidth());
+			btn7.setTranslateX(50);
+			btn7.setTranslateY(450);
+			
+			root.getChildren().addAll(list1, list2
+					, label1, label2, label3, btn1
+					, btn2, btn3, btn4, btn5, btn6, btn7);
+
+			
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private ArrayList<String> convert(java.util.List<FoodItem> list) {
+		ArrayList<String> foodList = new ArrayList<>(); 
+		for (int i = 0; i < list.size(); i++) {
+			foodList.add(list.get(i).getName());
+		}
+			 
+		 return foodList;
+	 }
+	
+	private List<FoodItem> sort (java.util.List<FoodItem> list) {
+		List<FoodItem> sortedList = new ArrayList<>();
+		 for (int i = 0; i < list.size(); i++) {
+			 FoodItem curFood = list.get(i);
+			 boolean notDone = true;
+			 for (int j = 0; j < sortedList.size(); j++) {
+				 if(curFood.getName().toLowerCase()
+						 .compareTo(sortedList.get(j).getName().toLowerCase()) < 0) {
+					 sortedList.add(j, curFood);
+					 notDone = false;
+					 System.out.println("WENT");
+					 break;
+				 }
+			 }
+			 if (notDone) {sortedList.add(curFood);}
+		 }
+		 return sortedList;
+	 }
+	
+	private void update (java.util.List<FoodItem> list) {
+		ObservableList<String> updateList = FXCollections.observableArrayList(convert(list));
+		list1.setItems(updateList);
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+	@Override
+	public void handle(ActionEvent event) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
